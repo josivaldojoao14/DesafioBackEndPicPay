1df775d5fde7b9f229e21715776256d1a62273d9
Josivaldo João <josivaldojoao14@gmail.com>
2023-10-27 11:23:31 -0300

    feat: initial configuration

---
+++ src/main/java/com/desafio/backend/picpay/DesafioBackEndPicPayApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.backend.picpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioBackEndPicPayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioBackEndPicPayApplication.class, args);
+	}
+
+}
