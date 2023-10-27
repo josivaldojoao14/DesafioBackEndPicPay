d787436d316696528abb9f265625fbdc3bd534eb
Josivaldo João <josivaldojoao14@gmail.com>
2023-10-27 11:24:29 -0300

    feat: user repository created

---
+++ src/main/java/com/desafio/backend/picpay/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.desafio.backend.picpay.repositories;
+
+import com.desafio.backend.picpay.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findUserByDocument(String document);
+    Optional<User> findUserByEmail(String email);
+}
