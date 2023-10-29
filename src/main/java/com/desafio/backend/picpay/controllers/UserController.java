d5a7de5b658f82b4b7c1bd5d9584f96fdb6280a3
Josivaldo João <josivaldojoao14@gmail.com>
2023-10-29 12:10:43 -0300

    feat: create UserController

---
+++ src/main/java/com/desafio/backend/picpay/controllers/UserController.java
@@ -0,0 +1,31 @@
+package com.desafio.backend.picpay.controllers;
+
+
+import com.desafio.backend.picpay.domain.user.User;
+import com.desafio.backend.picpay.dtos.UserDto;
+import com.desafio.backend.picpay.services.UserSevice;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(("/users"))
+public class UserController {
+    @Autowired
+    private UserSevice userSevice;
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody UserDto userDto){
+        User newUser = this.userSevice.createUser(userDto);
+        return new ResponseEntity<>(newUser, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> listUsers(){
+        List<User> usersList = this.userSevice.getAllUsers();
+        return new ResponseEntity<>(usersList, HttpStatus.OK);
+    }
+}
