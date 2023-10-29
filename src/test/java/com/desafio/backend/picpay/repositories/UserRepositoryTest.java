bcbf7b770c61fa6bed55e562c18dc5df96a40f6f
Josivaldo João <josivaldojoao14@gmail.com>
2023-10-29 12:16:49 -0300

    feat: create UserRepositoryTest

---
+++ src/test/java/com/desafio/backend/picpay/repositories/UserRepositoryTest.java
@@ -0,0 +1,20 @@
+package com.desafio.backend.picpay.repositories;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserRepositoryTest {
+
+    @Test
+    void findUserByDocument() {
+    }
+
+    @Test
+    void findUserByEmail() {
+    }
+
+    @Test
+    void findUserById() {
+    }
+}
